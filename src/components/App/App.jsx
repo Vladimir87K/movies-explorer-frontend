f339d40e5b31da6d3a816cb8d0dd5bb448bacc43
Владимир Куклин <privet1vovik@gmail.com>
2022-09-17 10:59:29 +0300

    footer без адаптации

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer'
+
+const App = () => {
+ 
+  return (
+    <div className='page'>
+      <Header />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+console.log(App);
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React  from 'react';
+//import { useNavigate } from "react-router-dom";
+import './Header.css';
+import logo from '../../images/logo.svg';
+import landing from '../../images/icon-accaunt.svg';
+
+const Header = (props) => {
+
+  return (
+    <header className="header">
+      <a href="#" className="header__link">
+        <img src={logo} alt="логотип" className="header__logo" />
+      </a>
+        <nav className='header__entrance'>
+          <button className='header__entrance_btn'>Регистрация</button>
+          <button className='header__entrance_btn'>Войти</button>
+        </nav>
+        <nav className='header__navigate'>
+          <a className='header__navigate_link' href='#'>Фильмы</a>
+          <a className='header__navigate_link' href='#'>Сохраненные фильмы</a>
+          <a className='header__navigate_link' href='#'>Аккаунт<img src={landing} alt="иконка" className='header__link_icon'/></a>
+        </nav>
+    </header>
+
+  );
+}
+
+export default Header;
