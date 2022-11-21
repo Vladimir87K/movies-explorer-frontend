e718fa833a77573d4c0e91d78cbc50a8ab3cb8b0
Владимир Куклин <privet1vovik@gmail.com>
2022-11-21 12:52:14 +0300

    ревью

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -72,7 +72,7 @@ class MainApi {
             })
         })
         .then(this._checkError)
-        // .then((res) => res.data)
+        .then((res) => res.data)
   }
 
   autorizationProfil(data) {
@@ -87,6 +87,7 @@ class MainApi {
             })
         })
         .then(this._checkError)
+        .then((res) => res.data)
   }
 
   correctProfil(data, token) {
